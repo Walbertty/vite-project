2e1ebe9beaa5752311fe2ed95744aaea5498eaca
Walbertty <walberttyg@gmail.com>
2022-12-14 08:20:16 -0300

    firt file added

---
+++ src/pages/Details.jsx
@@ -0,0 +1,11 @@
+//import { Fragment } from "react" or short version (<> </>)
+
+export function Details() {
+
+  return(
+    <>
+    <h1>My First App</h1>
+    <span>Walbertty Lopes</span>
+    </>
+  )
+}
