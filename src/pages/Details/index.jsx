8aade5df9e19d02d2456e963aa5c31df382210d5
Walbertty <walberttyg@gmail.com>
2023-01-04 08:30:39 -0300

    components Button, Header and Section added

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,17 +1,24 @@
 //import { Fragment } from "react" - Usado para desfragmentar mas não recebe estilização
 import { Container } from "./styles"
 
+import { Header } from "../../components/Header"
 import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
 
 export function Details() {
 
     return (
         <Container>
-        <h1>Hello Walbertty</h1>
-        <span>Walbertty Lopes</span>
+            <Header />
 
-            <Button title="Entrar" loading />
-            <Button title="Cadastrar" />
+            <Section tittle="Links úteis">
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </Section>
+
             <Button title="Voltar" />
         </Container>
     )
