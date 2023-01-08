e991ba133c49c5fe4b3eb6d1eac10e37929d9a59
Walbertty <walberttyg@gmail.com>
2023-01-08 08:32:11 -0300

    Folders Section and Home added

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 //import { Fragment } from "react" - Usado para desfragmentar mas não recebe estilização
-import { Container, Links } from "./styles"
+import { Container, Links, Content } from "./styles"
 
-import { Header } from "../../components/Header"
-import { Button } from "../../components/Button"
-import { Section } from "../../components/Section"
 import { Tag } from "../../components/Tag"
+import { Button } from "../../components/Button"
+import { Header } from "../../components/Header"
+import { Section } from "../../components/Section"
+import { ButtonText } from "../../components/ButtonText"
+
 
 export function Details() {
 
@@ -12,14 +14,21 @@ export function Details() {
         <Container>
             <Header />
 
+            <main>
+                <Content>
+                <ButtonText title="Excluir nota" />
+
+                <h1>Introdução ao React</h1>
+
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa assumenda sunt,
+                   pariatur doloribus aliquid quam. Explicabo sit perspiciatis dolore, sed expedita
+                   quidem culpa. Sint maxime consequuntur voluptate perferendis molestiae consectetur?
+                </p>
+
                 <Section title="Links úteis">
                     <Links>
-                        <li>
-                           <a href="#">Link 1</a>
-                        </li>
-                        <li>
-                           <a href="#">Link 2</a>
-                        </li>
+                        <li><a href="#">Link 1</a></li>
+                        <li><a href="#">Link 2</a></li>
                     </Links>
                 </Section>
 
@@ -29,6 +38,8 @@ export function Details() {
                 </Section>
 
             <Button title="Voltar" />
+                </Content>
+            </main>
         </Container>
     )
 }
