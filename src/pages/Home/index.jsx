a0bd6e774090785a1b95d4bae58346c570e1ff5a
Walbertty <walberttyg@gmail.com>
2023-01-17 09:41:27 -0300

    file Section and Note added

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiSearch } from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles'
 
+import { Note } from '../../components/Note'
+import { Input } from '../../components/Input'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
@@ -21,11 +25,20 @@ export function Home() {
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
             </Search>
 
             <Content>
-
+                <Section title="Minhas notas">
+                    <Note data={{ 
+                        title: 'React',
+                        tags: [
+                            {id: '1', name: 'react'},
+                            {id: '2', name: 'Nodejs'}
+                        ]
+                    }}
+                    />
+                </Section>
             </Content>
 
             <NewNote>
